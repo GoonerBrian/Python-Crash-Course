19ed14229330f80f236bf3ea2fea4351751d2f1f
GoonerBrian <brian.hoblin@gmail.com>
2020-05-09 15:17:10 -0400

    Added to dog.py

--- dog.py
+++ dog.py
@@ -7,8 +7,20 @@ class Dog():
 
     def sit(self):
         """Simulate a dog sitting in response to a command"""
-        print(self.name.title + " is now sitting.")
+        print(self.name.title() + " is now sitting.")
 
     def roll_over(self):
         """Simulate rolling over in response to a command"""
         print(self.name.title() + " rolled over!")
+
+
+my_dog = Dog('willie', 6)
+your_dog = Dog('lucy', 3)
+
+print("My dog's name is " + my_dog.name.title() + ".")
+print("My dog is " + str(my_dog.age) + " years old.")
+my_dog.sit()
+
+print("\nYour dog's name is " + your_dog.name.title() + ".")
+print("You dog is " + str(your_dog.age) + " years old")
+your_dog.sit()
